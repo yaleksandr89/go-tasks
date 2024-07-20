59387f7e4b42f90247fac9f7c41eb7288b4dbdd4
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2024-07-20 20:51:25 +0300

    complete s1-l10-4

---
+++ s1-l10-4/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	sum := 0
+	for i := 0; i < n; i++ {
+		var num int
+		fmt.Scan(&num)
+
+		if (num >= 10 && num <= 99) && num%8 == 0 {
+			sum += num
+		}
+	}
+
+	fmt.Println(sum)
+}
