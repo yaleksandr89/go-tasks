dbba37b5c8803d9228b7515b5ac64ec1c17fd2da
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2024-07-27 23:35:33 +0300

    complete s2-l1-7

--- section-2/l1-6/main.go
+++ section-2/l1-6/main.go
@@ -3,9 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	f("Hello!")
+	result := minimumFromFour()
+
+	fmt.Println(result)
 }
 
-func f(text string) {
-	fmt.Println(text)
+func minimumFromFour() int {
+	var a, b, c, d int
+	fmt.Scan(&a, &b, &c, &d)
+
+	if a < b && a < c && a < d {
+		return a
+	} else if b < a && b < c && b < d {
+		return b
+	} else if c < a && c < b && c < d {
+		return c
+	} else {
+		return d
+	}
 }
