bf9c2764a0916400feba32f82c97852d16c13fe4
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2024-07-27 23:36:57 +0300

    div l1-6 and l1-7

--- section-2/l1-6/main.go
+++ section-2/l1-6/main.go
@@ -3,22 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	result := minimumFromFour()
-
-	fmt.Println(result)
+	f("Hello!")
 }
 
-func minimumFromFour() int {
-	var a, b, c, d int
-	fmt.Scan(&a, &b, &c, &d)
-
-	if a < b && a < c && a < d {
-		return a
-	} else if b < a && b < c && b < d {
-		return b
-	} else if c < a && c < b && c < d {
-		return c
-	} else {
-		return d
-	}
+func f(text string) {
+	fmt.Println(text)
 }
